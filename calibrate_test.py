d47f191a4414488d82f9e95230583b2bda6b9fe3
Oleksandr Semeniuta <semenyuta@gmail.com>
2013-07-10 12:43:13 +0200

    Finished working on samples test in calibrate_test.py

--- calibrate_test.py
+++ calibrate_test.py
@@ -26,9 +26,38 @@ for i in range(len(found)):
         filtered_chessboard_corners_results.append(chessboard_corners_results[i])
         
 ''' Calibrate camera '''
-res, failures = calibration.calibrate_camera(filtered_images, pattern_size, square_size, filtered_chessboard_corners_results)
+res = calibration.calibrate_camera(filtered_images, pattern_size, square_size, filtered_chessboard_corners_results)
 camera_matrix = res[1]
 
-print failures
+print 'All images:'
 print camera_matrix
+
+''' Samples testing '''
+sample_size = 30
+num_of_tests = 3
+num_of_images_total = len(filtered_images)
+tests = []
+for i in range(num_of_tests):
+    sample = [random.randint(0, num_of_images_total - 1) for j in range(sample_size)]
+    tests.append(sample)
+    
+camera_matrices = []
+for t in tests:
+    sample_images = [filtered_images[el] for el in t]
+    sample_corners = [filtered_chessboard_corners_results[el] for el in t]
+    
+    print 'Sample %s:' % t
+    res = calibration.calibrate_camera(sample_images, pattern_size, square_size, sample_corners)
+    camera_matrix = res[1]
+    print camera_matrix    
+    
+    camera_matrices.append(np.array(camera_matrix))
+    
+print 'Averaged matrix:'
+avg_matrix = camera_matrices[0]
+for i in range(1, len(camera_matrices)):
+    avg_matrix += camera_matrices[i]
+avg_matrix /= num_of_tests
+print avg_matrix
+
 
