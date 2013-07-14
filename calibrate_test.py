a214158d94e7be7b2690fe9cab0e66c9aaf1ea30
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2013-07-14 12:23:11 +0200

    Code refactoring is performed (for further introduction of the command line arguments)

--- calibrate_test.py
+++ calibrate_test.py
@@ -1,17 +1,20 @@
 from cvexperiments import calibration_experiments as calexp
+import params
 
-''' Different image sets for testing '''
-first_four = (r'D:\Dropbox\SINTEF\img\first_four\*_0.bmp', (7, 7), 2.9, 'data/first_four.pickle', 'first_four')
-opencv_sample = (r'D:\Dropbox\SINTEF\img\opencv_sample\left*.jpg', (9, 6), 1.0, 'data/opencv_sample.pickle', 'opencv_sample')
-new_set_1 = (r'D:\Dropbox\SINTEF\img\Camera1-1\*.bmp', (8, 7), 2.9, 'data/new_set_1.pickle', 'new_set_left')
-new_set_2 = (r'D:\Dropbox\SINTEF\img\Camera2-1\*.bmp', (8, 7), 2.9, 'data/new_set_2.pickle', 'new_set_right')
+def initialize_parameters():
+    p = params.CalibrationExperiment    
+    
+    sample_size = p.sample_size
+    num_of_tests = p.num_of_tests    
+    images_mask, pattern_size, square_size, data_file, experiment_name = p.imageset
+    results_dir = p.results_dir
+    
+    return (images_mask, pattern_size, square_size, data_file, sample_size, num_of_tests, results_dir, experiment_name)
 
-''' Sample  testing parameters'''
-sample_size = 20
-num_of_tests = 2000
+def start_experiment(images_mask, pattern_size, square_size, data_file, sample_size, num_of_tests, results_dir, experiment_name):
+    calexp.different_samples_experiment(images_mask, pattern_size, square_size, data_file, sample_size, num_of_tests, results_dir, experiment_name)
+    
+if __name__ == '__main__':
+    parameters = initialize_parameters()
+    start_experiment(*parameters)
 
-images_mask, pattern_size, square_size, data_file, experiment_name = new_set_2
-
-results_dir = 'D:\Dropbox\SINTEF\experiments'
-
-calexp.different_samples_experiment(images_mask, pattern_size, square_size, data_file, sample_size, num_of_tests, results_dir, experiment_name)
