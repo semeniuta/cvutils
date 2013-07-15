8c56fe875c5d863cd74e82bcb79620c690e3c344
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2013-07-15 09:51:56 +0200

    Added code for creating histograms for the distortion coeffitients

--- cvexperiments/calibration_experiments.py
+++ cvexperiments/calibration_experiments.py
@@ -73,6 +73,11 @@ def different_samples_experiment(images_mask, pattern_size, square_size, data_fi
     fy = res_table_arr[:, 2]
     cx = res_table_arr[:, 3]
     cy = res_table_arr[:, 4]
+    k1 = res_table_arr[:, 5]
+    k2 = res_table_arr[:, 6]
+    p1 = res_table_arr[:, 7]
+    p2 = res_table_arr[:, 8]
+    k3 = res_table_arr[:, 9]
  
     nbins = 100
     
@@ -80,6 +85,11 @@ def different_samples_experiment(images_mask, pattern_size, square_size, data_fi
     create_and_save_histogram(fy, nbins, 'fy', os.path.join(results_dir, 'fy_hist.png'))
     create_and_save_histogram(cx, nbins, 'cx', os.path.join(results_dir, 'cx_hist.png'))
     create_and_save_histogram(cy, nbins, 'cy', os.path.join(results_dir, 'cy_hist.png'))
+    create_and_save_histogram(k1, nbins, 'k1', os.path.join(results_dir, 'k1_hist.png'))
+    create_and_save_histogram(k2, nbins, 'k2', os.path.join(results_dir, 'k2_hist.png'))
+    create_and_save_histogram(p1, nbins, 'p1', os.path.join(results_dir, 'p1_hist.png'))
+    create_and_save_histogram(p2, nbins, 'p2', os.path.join(results_dir, 'p2_hist.png'))
+    create_and_save_histogram(k3, nbins, 'k3', os.path.join(results_dir, 'k3_hist.png'))
             
 def write_calibration_results_to_file(res_table, f): 
     columns = ['rms', 'fx', 'fy', 'cx', 'cy', 'k1', 'k2', 'p1', 'p2', 'k3']    
